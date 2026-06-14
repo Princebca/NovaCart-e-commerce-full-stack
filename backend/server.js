9b69a332d5bf963810794a030a92af376edf3e41
Princebca <bishwakarmaprince081@gmail.com>
2026-06-14 14:05:35 +0530

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,9 @@ connectCloudinary()
 app.use(express.json())
 app.use(cors({
   origin: [
-    'http://localhost:5173',
-    'https://nova-cart-frontend.vercel.app'
+    "https://nova-cart-frontend.vercel.app",
+    "https://nova-cart-admin-panel.vercel.app",
+    "http://localhost:5173"
   ],
   credentials: true
 }))
